71f0bd096aca853b195b37f74f6143ca3fa2c12c
Haoran Sun <hsun3@uw.edu>
2022-02-25 12:22:26 -0800

    Complete demo_did.py!

--- src/demo_did.py
+++ src/demo_did.py
@@ -4,6 +4,15 @@ import logging
 
 __log = logging.getLogger(__name__)
 
+def root_file_extractor(url):
+    page = urlopen(url) 
+    #Read and decode the page from byts to string
+    decoded_page = page.read().decode("utf-8") 
+    pattern ="=\"(.+?)\">out" #the pattern of the string we want: link ending with .root
+    match_results = re.findall(pattern, decoded_page)
+    complete_root_file = [url + match_result for match_result in match_results]
+    return complete_root_file
+
 async def find_files(did_name: str, info: Dict[str, Any]) -> AsyncGenerator[Dict[str, Any], None]:
     '''For each incoming did name, generate a list of files that ServiceX can process
 
@@ -15,18 +24,21 @@ async def find_files(did_name: str, info: Dict[str, Any]) -> AsyncGenerator[Dict
     Returns:
         AsyncGenerator[Dict[str, any], None]: yield each file
     '''
+    root_file = root_file_extractor(did_name) # Finding files
+
     __log.info('Looking up dataset {did_name}',
                       extra={'requestId': info['request-id']})
 
     if did_name != 'dataset1':
         raise Exception(f'Dataset "{did_name}" is not known!')
 
-    yield {
-        'file_path': "http://opendata.cern.ch/record/3827/files/mc_167740.WenuWithB.root",  # Path accessible via transformers (root, http)
-        'adler32': 0,  # No clue
-        'file_size': 0,  # Size in bytes if known
-        'file_events': 0,  # Number of events if known
-    }
+    for file in root_file:
+        yield {
+            'file_path': "http://opendata.cern.ch/record/3827/files/mc_167740.WenuWithB.root",  # Path accessible via transformers (root, http)
+            'adler32': 0,  # No clue
+            'file_size': 0,  # Size in bytes if known
+            'file_events': 0,  # Number of events if known
+        }
 
 def run_demo():
     log = logging.getLogger(__name__)
